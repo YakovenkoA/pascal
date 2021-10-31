4c4bf12067559c8afcb6edf4b19e5ff276e3c97f
YakovenkoA <aleksandrakovenko21@gmail.com>
2021-10-31 20:48:44 +0500

    number_rank

--- number_rank.pas
+++ number_rank.pas
@@ -1,2 +1,15 @@
 program number_rank;
 var a,b,c,d,s: integer;
+begin
+    writeln ('Введите трехзначное число: ');
+    read (a);
+    b:= a div 100;
+    d:= a mod 10;
+    c:= a mod 100;
+    c:= c div 10;
+    s:= d+b+c;
+    writeln ('Сотни ', b);
+    writeln ('Десятки ', c);
+    writeln ('Единицы ', d);
+    writeln ('Сумма ', s);
+end.    
