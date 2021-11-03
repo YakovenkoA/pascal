54589a94c8417782e6eb20076aac33935bed4a56
YakovenkoA <aleksandrakovenko21@gmail.com>
2021-11-03 15:51:34 +0500

    fondation_date

---
+++ foundation_date.pas
@@ -0,0 +1,8 @@
+program foundation;
+var A:integer;
+begin
+    write ('Введите год осноавния Санкт-Питербурга: ');
+    read (A);
+    if (A=1703) then write ('Верно!!!')
+    else write ('Вы ошиблись!!!');
+end.
