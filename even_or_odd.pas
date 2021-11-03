b7f8a9db8c6924bde53f80a538e6f65e08397b67
YakovenkoA <aleksandrakovenko21@gmail.com>
2021-11-03 19:21:32 +0500

    even_odd_1

--- even_or_odd.pas
+++ even_or_odd.pas
@@ -1,11 +1,8 @@
 program even_odd;
-var a: integer;
-var c, b: integer;
+var a:integer;
 begin
     write ('Enter number: ');
     read (a);
-    b:=a/2;
-    c:= b mod 1;
-    if (c=0) then write ('even') 
+    if (a mod 2 = 0) then write ('Even')
     else write ('odd');
 end.
