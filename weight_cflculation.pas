0abede647055f16144ec5949d0aa3e97f5654591
YakovenkoA <aleksandrakovenko21@gmail.com>
2021-11-03 23:01:15 +0500

    errrorrr

--- weight_cflculation.pas
+++ weight_cflculation.pas
@@ -1,5 +1,5 @@
 program weight_calculation;
-var h, m, opt: integer;
+var h, m, opt, d: integer;
 begin
     write ('Enter height (cm): ');
     read (h);
@@ -8,7 +8,7 @@ begin
     opt:= h-100;
     if (m = opt) then write
      ('You have optimal weight.')
-    else if (m < opt) then write 
-    ('You should get better.') else 
-     write ('You should lose weight.');
+    else if (m < opt) then d:= opt-m write 
+    ('You should get better: ',d ,'kg') else 
+    d:= m-opt write ('You should lose weight: ',d ,'kg');
 end.
